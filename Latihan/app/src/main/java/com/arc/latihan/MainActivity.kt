231af38bbb7ce666a8967cbff62fd787e0dd0d1d
AbyanRamzi <abyanramzi311000@gmail.com>
2022-10-18 16:27:46 +0700

    Hello Abyan

--- Latihan/app/src/main/java/com/arc/latihan/MainActivity.kt
+++ Latihan/app/src/main/java/com/arc/latihan/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Abyan")
+                    Greeting("Abyan Ramzi")
                 }
             }
         }
@@ -38,6 +38,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     LatihanTheme {
-        Greeting("Abyan")
+        Greeting("Abyan Ramzi")
     }
 }
